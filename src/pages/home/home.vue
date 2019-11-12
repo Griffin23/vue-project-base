3c0d9849a1ad3d57bdc1531d03ee1855f50f8230
Vellanueva <838884524@qq.com>
2019-11-12 16:44:00 +0800

    chore: rwd方案

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>haha</div>
+  <div>
+    <div class="griffin23-container foo-container">
+      hoho
+    </div>
+    <div class="small-screen-hide">
+      测试小屏隐藏
+    </div>
+  </div>
 </template>
 <script></script>
-<style></style>
+<style lang="scss" scoped>
+  @import "../../assets/css/global";
+
+  .foo-container {
+    @include large-screen-style {
+      background-color: red;
+    }
+    @include small-screen-style {
+      background-color: yellow;
+    }
+  }
+</style>
