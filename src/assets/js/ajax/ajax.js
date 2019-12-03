4a06972c8ff92f2b019b8ac6954161250dd6d78f
Vellanueva <838884524@qq.com>
2019-12-03 09:22:26 +0800

    chore: set ajax default headers

--- src/assets/js/ajax/ajax.js
+++ src/assets/js/ajax/ajax.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 
 import { getOrigin } from '../util.js';
 
+const INIT_AXIOS_HEADERS = {
+  'Pragma': 'no-cache', // ajax http get result is forced cached by IE, so use no-cache header
+  'Cache-Control': 'no-cache'
+};
+
 /***
  * axios返回数据处理器
  * @param successHttpCode 判定为成功的http code
@@ -22,7 +27,11 @@ function axiosRespHandler(successHttpCode, response, successCb) {
 
 function Ajax() {
   this.get = function(url, successCb, errorCb) {
-    axios.get(url)
+    axios({
+      url: url,
+      method: 'get',
+      headers: INIT_AXIOS_HEADERS
+    })
       .then((response) => {
         axiosRespHandler(200, response, successCb);
       })
