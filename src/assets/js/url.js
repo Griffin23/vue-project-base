e0f310da521fe8a8b6b6e5158e969ae55d16d7da
Vellanueva <838884524@qq.com>
2019-12-03 11:43:14 +0800

    feat: api urls

--- src/assets/js/url.js
+++ src/assets/js/url.js
@@ -1,3 +1,15 @@
-// 接口地址汇总
+const BASE_URLS = {
+  development: {
+    XXX_API: 'xxxApiDevBaseUrl'
+  },
+  test: {
+    XXX_API: 'xxxApiTestBaseUrl'
+  },
+  production: {
+    XXX_API: 'xxxApiProdBaseUrl'
+  }
+};
 
-export const xxApiUrl = '';
+export const XXX_API_BASE_URL = BASE_URLS[process.env.NODE_ENV].XXX_API;
+
+export const LOGIN_URL = `${XXX_API_BASE_URL}/login`;
