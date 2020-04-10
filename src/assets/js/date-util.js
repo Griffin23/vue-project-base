dfaf7b2fd3091044489b2ee2ce55c8288f83c097
Vellanueva <838884524@qq.com>
2020-04-10 11:37:02 +0800

    根据日期差（天数）获取日期

---
+++ src/assets/js/date-util.js
@@ -0,0 +1,9 @@
+/***
+ * 根据日期差（天数）获取日期
+ * @param diffDay
+ * @returns {Date}
+ */
+export function getDateByDiffDay(diffDay) {
+  let time = new Date().getTime() + diffDay * 24 * 60 * 60 * 1000;
+  return new Date(time);
+}
