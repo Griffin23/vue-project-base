80facb9856ace429f577ca46826e528721e688e4
Vellanueva <838884524@qq.com>
2021-04-09 21:44:27 +0800

    style: util.js - remove semicolon

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -5,78 +5,80 @@
  */
 
 export function isDev() {
-  return process.env.NODE_ENV === 'development';
+  return process.env.NODE_ENV === 'development'
 }
 
 export function isTest() {
-  return process.env.NODE_ENV === 'test';
+  return process.env.NODE_ENV === 'test'
 }
 
 export function isProd() {
-  return process.env.NODE_ENV === 'production';
+  return process.env.NODE_ENV === 'production'
 }
 
 export function isJsFunction(fn) {
-  return typeof fn === 'function';
+  return typeof fn === 'function'
 }
 
 // region browser
 
 export function getQueryString(name) {
-  let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
-  let r = window.location.search.substr(1).match(reg);
+  let reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)')
+  let r = window.location.search.substr(1).match(reg)
   if (r != null) {
-    return decodeURI(r[2]);
+    return decodeURI(r[2])
   }
-  return null;
+  return null
 }
 
 export function getCookie(key) {
-  let cookiePairs = document.cookie.split(';');
+  let cookiePairs = document.cookie.split(';')
   for (let i = 0; i < cookiePairs.length; i++) {
     // seperate key value by equal operator(=). Note that value may contain equal operator
-    let equalOperatorIndex = cookiePairs[i].indexOf('=');
+    let equalOperatorIndex = cookiePairs[i].indexOf('=')
     // must use trim() to remove spaces
-    let currKey = cookiePairs[i].substring(0, equalOperatorIndex).trim();
+    let currKey = cookiePairs[i].substring(0, equalOperatorIndex).trim()
     if (currKey === key) {
-      return decodeURIComponent(cookiePairs[i].substring(equalOperatorIndex + 1));
+      return decodeURIComponent(cookiePairs[i].substring(equalOperatorIndex + 1))
     }
   }
-  return '';
+  return ''
 }
+
 export function setCookie(key, value, expire_ms) {
-  let result = `${key}=${encodeURIComponent(value)}`;
+  let result = `${key}=${encodeURIComponent(value)}`
   if (expire_ms) {
-    result += `;expires=${(new Date(new Date().getTime() + expire_ms)).toUTCString()}`;
+    result += `;expires=${(new Date(new Date().getTime() + expire_ms)).toUTCString()}`
   }
-  document.cookie = result;
+  document.cookie = result
 }
+
 export function removeCookie(key) {
-  setCookie(key, '', -1);
+  setCookie(key, '', -1)
 }
 
 // 当前页打开链接
 export function openLinkThisTab(link) {
   if (link) {
-    window.location.href = link;
+    window.location.href = link
   }
 }
 
 // 新标签页打开链接
 export function openLinkNewTab(link) {
   if (link) {
-    window.open(link);
+    window.open(link)
   }
 }
 
 export function getOrigin() {
-  let location = window.location;
-  let origin = location.origin;
+  let location = window.location
+  let origin = location.origin
   if (origin === undefined) {
     return `${location.protocol}//${location.hostname}${(location.port
-      ? `:${location.port}` : '')}`;
+      ? `:${location.port}` : '')}`
   }
-  return origin;
+  return origin
 }
 
 /***
@@ -90,30 +92,31 @@ export function getOrigin() {
  *    false，需要在组件每次show时注册，原理是维持一个回调函数栈，每次esc键按下时，
  *        只会执行栈顶的回调函数，这样可以解决一次esc隐藏所有dom的问题。
  */
-let escCbStack = [];
-let hasRegisterEscKeyDownEventListener = false;
+let escCbStack = []
+let hasRegisterEscKeyDownEventListener = false
+
 export function registerEscKeyDown(cb, isPermenantReg) {
   if (isPermenantReg) {
     window.addEventListener('keydown', (evt) => {
       if ((evt.key === 'Escape' || evt.key === 'Esc') && isJsFunction(cb) && escCbStack.length === 0) {
-        cb();
+        cb()
       }
-    });
+    })
   } else {
     if (isJsFunction(cb)) {
-      escCbStack.push(cb);
+      escCbStack.push(cb)
     }
     if (!hasRegisterEscKeyDownEventListener) {
       // 只注册一次
-      hasRegisterEscKeyDownEventListener = true;
+      hasRegisterEscKeyDownEventListener = true
       window.addEventListener('keydown', (evt) => {
         if (evt.key === 'Escape' || evt.key === 'Esc') {
-          let curCb = escCbStack.pop();
+          let curCb = escCbStack.pop()
           if (isJsFunction(curCb)) {
-            curCb();
+            curCb()
           }
         }
-      });
+      })
     }
   }
 }
@@ -132,14 +135,14 @@ export function registerEscKeyDown(cb, isPermenantReg) {
  * @returns {Function}
  */
 export function debounce(fn, interval = 100) {
-  let timeout = null;
-  let args = [].slice.call(arguments, 2);
+  let timeout = null
+  let args = [].slice.call(arguments, 2)
   return function() {
-    clearTimeout(timeout);
+    clearTimeout(timeout)
     timeout = setTimeout(() => {
-      fn.apply(this, args);
-    }, interval);
-  };
+      fn.apply(this, args)
+    }, interval)
+  }
 }
 
 // endregion
@@ -150,38 +153,38 @@ export function debounce(fn, interval = 100) {
  * insert new element after ref element
  */
 export function insertAfter(newEle, refEle) {
-  let parent = refEle.parentNode;
+  let parent = refEle.parentNode
   if (parent.lastChild === refEle) {
-    parent.appendChild(newEle);
+    parent.appendChild(newEle)
   } else {
-    parent.insertBefore(newEle, refEle.nextSibling);
+    parent.insertBefore(newEle, refEle.nextSibling)
   }
 }
 
 export function hasCssClass(ele, clazzName) {
-  return !!ele.className.match(new RegExp("(\\s|^)" + clazzName + "(\\s|$)"));
+  return !!ele.className.match(new RegExp("(\\s|^)" + clazzName + "(\\s|$)"))
 }
 
 export function addCssClass(ele, clazzName) {
   if (!hasCssClass(ele, clazzName)) {
-    ele.className += " " + clazzName;
+    ele.className += " " + clazzName
   }
 }
 
 export function removeCssClass(ele, clazzName) {
   if (hasCssClass(ele, clazzName)) {
-    ele.className = ele.className.replace(new RegExp("(\\s|^)" + clazzName + "(\\s|$)"), " ");
+    ele.className = ele.className.replace(new RegExp("(\\s|^)" + clazzName + "(\\s|$)"), " ")
   }
 }
 
 // 固定底部背景（如遮罩层的出现）
 export function fixBackground() {
-  document.body.style.overflow = 'hidden';
+  document.body.style.overflow = 'hidden'
 }
 
 // 取消固定底部背景（如遮罩层的隐藏）
 export function cancelFixBackground() {
-  document.body.style.overflow = '';
+  document.body.style.overflow = ''
 }
 
 // endregion
